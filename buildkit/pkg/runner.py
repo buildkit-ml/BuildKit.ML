75e5bdddf8756c571d04500e42d14024385b3e66
Xiaozhe Yao <askxzyao@gmail.com>
2022-10-25 18:03:14 +0200

    update

--- buildkit/pkg/runner.py
+++ buildkit/pkg/runner.py
@@ -1,9 +1,6 @@
 import os
 from typing import Dict
-from loguru import logger
-import requests
 from spython.main import Client
-
 
 class Runner:
     def __init__(self):
@@ -34,21 +31,6 @@ class Runner:
         ):
             print(line, end='')
 
-    def run_pipeline(self, config: Dict, image_path=None, webhook=""):
-        if webhook != "":
-            # update status to be running
-            res = requests.post(webhook, json={
-                "type": "general",
-                "status": "running",
-            })
+    def run_pipeline(self, config: Dict, image_path=None):
         for command in config['pipeline']:
            self.run_command(config, command, image_path)
-        if webhook != "":
-            # find results and push to toma
-            res = requests.post(webhook, json={
-                "type": "general",
-                "status": "finished",
-                "returned_payload": {
-                    "files": os.listdir("results/")
-                },
-            })
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author="Xiaozhe Yao",
     author_email="askxzyao@gmail.com",
     description="Run ML models within containerized, rootless and immutable environment",
-    version='0.0.5',
+    version='0.0.1',
     scripts=['buildkit/cli/bkm'],
     package_dir={'buildkit': 'buildkit'},
     packages=find_packages(),
